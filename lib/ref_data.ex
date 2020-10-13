6ccda18d5392bcb3ce02a100ad517ce83f15d047
Andrew Barr <andrew@foxtail.consulting>
2020-10-13 10:27:35 +1000

    updating documentation

--- lib/ref_data.ex
+++ lib/ref_data.ex
@@ -9,7 +9,30 @@ defmodule RefData do
   defmacro __using__(_opts) do
 
     quote do
-      @doc """
+
+      def list_all_keys() do
+        GenServer.call(RefData.Server, {:all_keys})
+      end
+
+      def get(key) do
+        GenServer.call(RefData.Server, key)
+      end
+
+      def get(key, :raw) do
+        GenServer.call(RefData.Server, {key, :raw})
+      end
+
+      def get(key, disabled: list) do
+        GenServer.call(RefData.Server, {key, disabled: list})
+      end
+
+      def get(key, _), do: get(key)
+
+    end
+  end
+
+
+  @doc """
       Returns a list of all key values from the underlying data store.
       It will match the keys used in the individual json files
 
@@ -19,11 +42,9 @@ defmodule RefData do
           ["key1", "key2"]
 
       """
-      def list_all_keys() do
-        GenServer.call(RefData.Server, {:all_keys})
-      end
+  @callback list_all_keys() :: List
 
-      @doc """
+  @doc """
       Returns a list of data for the provided key. If the json defines
       grouped data it will return grouped data.
 
@@ -48,9 +69,7 @@ defmodule RefData do
           ]
 
       """
-      def get(key) do
-        GenServer.call(RefData.Server, key)
-      end
+      @callback get(key :: String) :: List
 
       @doc """
       Returns teh raw data stored in memory for the provided key.
@@ -64,9 +83,7 @@ defmodule RefData do
           }
         ]
       """
-      def get(key, :raw) do
-        GenServer.call(RefData.Server, {key, :raw})
-      end
+      @callback get(key :: String, params :: keyword()) :: List
 
       @doc """
       You can disable values in the data by providing a list of
@@ -81,18 +98,7 @@ defmodule RefData do
           [key: "Non-binary", value: "non-binary"]
         ]
       """
-      def get(key, disabled: list) do
-        GenServer.call(RefData.Server, {key, disabled: list})
-      end
-
-      @doc """
-      Any unrecognised key will default to RefData.get/1
-      """
-      def get(key, _), do: get(key)
-
-    end
-  end
-
+      @callback get(String, disabled: List) :: List
 
 
 end
