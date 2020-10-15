c3cd08f8566ba0192dae0659bcbdd2281a242c91
Andrew Barr <andrew@foxtail.consulting>
2020-10-15 14:37:16 +1000

    modified server file

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RefData.MixProject do
   def project do
     [
       app: :ref_data,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
